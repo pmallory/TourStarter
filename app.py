bd3400b585a52b72c00060e8c1aeedc7c36a0ff2
pmallory <pmallory@altalang.com>
2012-11-21 11:01:12 -0500

    only run in debug mode if we're running locally.

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ if __name__ == '__main__':
     waypoints = load_waypoints()
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port, debug=True)
+    app.run(host='0.0.0.0',port=port, debug=os.path.isfile('dev'))
