82fc0d865b3bc8e9c0831fa6c40de2a3672200f3
pmallory <pmallory@altalang.com>
2012-11-27 16:24:55 -0500

    pep8

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ if __name__ == '__main__':
     waypoints = load_waypoints()
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port, debug=os.path.isfile('dev'))
+    app.run(host='0.0.0.0', port=port, debug=os.path.isfile('dev'))
 
