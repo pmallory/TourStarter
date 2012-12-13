fa9c58e40b00de3815da60a9dd25f69a1bc9ffd5
pmallory <pmallory@altalang.com>
2012-12-13 14:10:03 -0500

    add great divide to kml

--- static/js/TourStarter.js
+++ static/js/TourStarter.js
@@ -26,7 +26,7 @@ function initialize() {
     bikeLayer.setMap(map);
 
     // TODO should be relative to $SCRIPTROOT
-    var overlay = new google.maps.KmlLayer('http://rocky-citadel-8652.herokuapp.com/static/routes/cta21.kml');
+    var overlay = new google.maps.KmlLayer('http://rocky-citadel-8652.herokuapp.com/static/routes/cta22.kml');
     overlay.setMap(map);
 
     initializeOverlays();
