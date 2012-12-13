4709055db04acc830f842e9df147588482da1388
pmallory <pmallory@altalang.com>
2012-12-13 11:47:21 -0500

    add pacific coast to KML

--- static/js/TourStarter.js
+++ static/js/TourStarter.js
@@ -26,7 +26,7 @@ function initialize() {
     bikeLayer.setMap(map);
 
     // TODO should be relative to $SCRIPTROOT
-    var overlay = new google.maps.KmlLayer('http://rocky-citadel-8652.herokuapp.com/static/routes/cta15.kml');
+    var overlay = new google.maps.KmlLayer('http://rocky-citadel-8652.herokuapp.com/static/routes/cta16.kml');
     overlay.setMap(map);
 
     initializeOverlays();
