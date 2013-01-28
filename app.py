a4c8ee82434a5a8dd0cea09a4a852b69821f13b4
pmallory <philip@pmallory.com>
2013-01-27 21:49:39 -0500

    add About page

--- app.py
+++ app.py
@@ -77,6 +77,10 @@ def nearest_waypoint():
 def index():
     return render_template('index.html')
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=os.path.isfile('dev'))
